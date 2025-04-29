62d63f85cba1cb88e06c05b1fada6daf11c04cfb
Soc-zlincoln <zlincoln@storans.school.nz>
2025-04-29 14:06:41 +1200

    start
    label and comments

---
+++ as92004_zoe-_l.py
@@ -0,0 +1,22 @@
+"""
+DIGI Internal Quiz.
+By zoe lincoln
+Starting Date:29/04/2025
+"""
+
+#says hello to the user asking if they want to play a game.
+
+
+
+
+
+#gives thems 5 questions with true or false answers.
+
+# give them 20 point for every question they get right.
+
+
+
+
+
+#tells them their score out of 100.
+
