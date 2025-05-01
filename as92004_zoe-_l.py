79edc848c476d46839032f476b1ad30fd2bb74ba
Soc-zlincoln <zlincoln@storans.school.nz>
2025-05-01 15:05:15 +1200

    Update as92004_zoe-_l.py
    variables, asking if they wanted to play the game and the farewell code.

--- as92004_zoe-_l.py
+++ as92004_zoe-_l.py
@@ -3,20 +3,26 @@ DIGI Internal Quiz.
 By zoe lincoln
 Starting Date:29/04/2025
 """
-
-#says hello to the user asking if they want to play a game.
-
-
-
-
-
-#gives thems 5 questions with true or false answers.
-
-# give them 20 point for every question they get right.
+#variables
+score = 0
+name = input("What's your name?")
+start_game = input("Would you like to play a true or false Maori colour quiz?")
 
 
+#says hello to the user asking if they want to play a game
 
 
+if start_game == "yes":
+  print("Great let's get started!! The way you play is You will be asked if the Maori translastion lines up with the english translastion of the colour.")
+else:
+  print("Ok bye-bye.")
+  
+#gives thems 5 questions with true or false answers
 
-#tells them their score out of 100.
 
+ 
+#farewel the player
+#tells them their score out of 100
+
+print("Good job you did it!!")
+print("You got", score, "out of 100!!")
