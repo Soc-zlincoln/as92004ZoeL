6df854b2b7e75f3d71a8501a37caca70a177bde0
Soc-zlincoln <zlincoln@storans.school.nz>
2025-05-05 14:10:15 +1200

    Update as92004_zoe-_l.py
    questions 1-3

--- as92004_zoe-_l.py
+++ as92004_zoe-_l.py
@@ -8,17 +8,34 @@ score = 0
 name = input("What's your name?")
 start_game = input("Would you like to play a true or false Maori colour quiz?")
 
-
 #says hello to the user asking if they want to play a game
 
-
-if start_game == "yes":
-  print("Great let's get started!! The way you play is You will be asked if the Maori translastion lines up with the english translastion of the colour.")
+if start_game == "yes" :
+  print("Great", name,"let's get started!! The way you play is You will be asked if the Maori translastion lines up with the english translastion of the colour.")
 else:
   print("Ok bye-bye.")
-  
-#gives thems 5 questions with true or false answers
 
+#gives thems 5 questions with true or false answers
+input("Whero is the Maori word for Red. True or False?")
+if True :
+  print("Correct! Whero is the Maori word for Red.")
+  score = +20
+else:
+  print("Incorrect! Try and remember for next time.")
+
+input("Kowhai is the Maori for Blue. True or False?")
+if False :
+  print("Correct! Kowhai is Maori for Yellow not Blue.")
+  score = +20
+else:
+  print("Incorrect! Kowhai isn't Blue.")
+
+input("Kakariki is the Maori word for Green. True or False?")
+if True :
+  print("Correct! Kakariki is the Maori word for Green.")
+  score = +20
+else:
+  print("Incorrect! Try and remember for next time.")
 
  
 #farewell the player
@@ -26,3 +43,4 @@ else:
 
 print("Good job you did it!!")
 print("You got", score, "out of 100!!")
+
