b7975116722223a0529ad0a0231ca11573947992
Soc-zlincoln <zlincoln@storans.school.nz>
2025-05-06 12:40:32 +1200

    Update as92004_zoe-_l.py
    fixed questions and answers to work need to fix scoring

--- as92004_zoe-_l.py
+++ as92004_zoe-_l.py
@@ -7,8 +7,8 @@ Starting Date:29/04/2025
 score = 0
 name = input("What's your name?")
 start_game = input("Would you like to play a true or false Maori colour quiz?")
-question1 = ["Whero is the Maori word for Red. True or False?", "Kowhai is the Maori for Blue. True or False?","Kakariki is the Maori word for Green. True or False?","Ma Whero is the Maori word for White. True or False?","Pango is the Maori word for Black. True or False?"]
-answer = ["True","False","True","False","True"]
+question = ["Whero is the Maori word for Red. True or False?", "Kowhai is the Maori for Blue. True or False?","Kakariki is the Maori word for Green. True or False?","Ma Whero is the Maori word for White. True or False?","Pango is the Maori word for Black. True or False?"]
+answer = ["true","false","true","false","true"]
 
 #says hello to the user asking if they want to play a game
 
@@ -18,21 +18,17 @@ else:
   print("Ok bye-bye.")
 
 #gives thems 5 questions with true or false answers
-for x in question1:
-  if x == user_answer:
-    break
-  print(x)
+for x in range(len(question)):
+  user_answer = input(question[x])
 
+  if user_answer == answer[x]:
+    print("Correct!! Well done.")
+    score = +20
+  else:
+    print("Incorrect!! Remember for next time.")
  
 #farewell the player
 #tells them their score out of 100
 
 print("Good job you did it!!")
 print("You got", score, "out of 100!!")
-
-
-
-
-
-
-
